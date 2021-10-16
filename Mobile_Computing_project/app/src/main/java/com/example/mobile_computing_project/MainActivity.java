e1f5364b3cc25a6f52b89ee0c436f9a0d785caa4
preshiousshittu <54418932+preshiousshittu@users.noreply.github.com>
2021-10-15 22:25:36 -0500

    basic navigation to cart
    adds a button that allows you to navigate to cart

--- Mobile_Computing_project/app/src/main/java/com/example/mobile_computing_project/MainActivity.java
+++ Mobile_Computing_project/app/src/main/java/com/example/mobile_computing_project/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mobile_computing_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+    public void ShowCart(View v){
+        Intent ini = new Intent(this, startpage.class);
+        startActivity(ini);
+
     }
 }
