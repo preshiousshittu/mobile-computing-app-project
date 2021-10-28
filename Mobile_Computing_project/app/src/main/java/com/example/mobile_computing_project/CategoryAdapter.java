1e966aadd48ec5716573a29d1d98576d8d57f598
preshiousshittu <54418932+preshiousshittu@users.noreply.github.com>
2021-10-28 10:43:49 -0500

    Editted store page layouts and create recycler view
    edited widgets in the main xml and created a java class for my recycler adapter

---
+++ Mobile_Computing_project/app/src/main/java/com/example/mobile_computing_project/CategoryAdapter.java
@@ -0,0 +1,12 @@
+package com.example.mobile_computing_project;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
