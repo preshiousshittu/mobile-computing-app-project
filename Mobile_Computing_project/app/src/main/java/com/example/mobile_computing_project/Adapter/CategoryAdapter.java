cd18003f6bac317ba739c6a3e799e09914822bb0
preshiousshittu <54418932+preshiousshittu@users.noreply.github.com>
2021-10-28 17:34:55 -0500

    completes Category Domian and CategoryAdapter

--- Mobile_Computing_project/app/src/main/java/com/example/mobile_computing_project/Adapter/CategoryAdapter.java
+++ Mobile_Computing_project/app/src/main/java/com/example/mobile_computing_project/Adapter/CategoryAdapter.java
@@ -1,27 +1,54 @@
 package com.example.mobile_computing_project.Adapter;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mobile_computing_project.Domain.CategoryDomain;
+import com.example.mobile_computing_project.R;
 
 import java.util.ArrayList;
 
-public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter> {
-    ArrayList<>
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+    ArrayList<CategoryDomain> categoryDomains;
+
+    public CategoryAdapter(ArrayList<CategoryDomain> categoryDomains) {
+        this.categoryDomains = categoryDomains;
+    }
+
     @NonNull
     @Override
-    public CategoryAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public  ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_cat, parent, false);
+        return new ViewHolder(inflate);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CategoryAdapter holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.categoryName.setText(categoryDomains.get(position).getTitle());
     }
+
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return categoryDomains.size();
+    }
+
+    public class  ViewHolder extends RecyclerView.ViewHolder{
+        TextView categoryName;
+        ConstraintLayout mainLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            categoryName = itemView.findViewById(R.id.categoryName);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+        }
     }
 }
