3c35b74b960cc679e7242fd0d95552feee82c1c4
Adesope <S533584@nwmissouri.edu>
2021-10-29 23:21:59 -0500

    Add Fragments to StartActivity page

--- Mobile_Computing_project/app/src/main/java/com/example/mobile_computing_project/Fragments/signUpFragment.java
+++ Mobile_Computing_project/app/src/main/java/com/example/mobile_computing_project/Fragments/signUpFragment.java
@@ -40,16 +40,12 @@ public class signUpFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment signUpFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static signUpFragment newInstance(String param1, String param2) {
+    public static signUpFragment newInstance() {
         signUpFragment fragment = new signUpFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -66,8 +62,7 @@ public class signUpFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
