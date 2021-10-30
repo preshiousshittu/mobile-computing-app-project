9c76e1acea6346daf8d42c71918028d641addc9c
Adesope <S533584@nwmissouri.edu>
2021-10-29 22:53:26 -0500

    Add Fragment for SignUp

--- Mobile_Computing_project/app/src/main/java/com/example/mobile_computing_project/Fragments/signUpFragment.java
+++ Mobile_Computing_project/app/src/main/java/com/example/mobile_computing_project/Fragments/signUpFragment.java
@@ -1,12 +1,15 @@
 package com.example.mobile_computing_project.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.mobile_computing_project.R;
 
@@ -16,7 +19,7 @@ import com.example.mobile_computing_project.R;
  * create an instance of this fragment.
  */
 public class signUpFragment extends Fragment {
-    public interface switchCallBack {
+    public interface SignUpCallBack {
         public void swapToSignIn();
     }
 
@@ -51,6 +54,14 @@ public class signUpFragment extends Fragment {
         return fragment;
     }
 
+    private SignUpCallBack activity = null;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        activity = (SignUpCallBack) context;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,6 +75,15 @@ public class signUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View v = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        Button SUsignInbtn = v.findViewById(R.id.SUsignInbtn);
+        SUsignInbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.swapToSignIn();
+            }
+        });
+        return v;
     }
 }
