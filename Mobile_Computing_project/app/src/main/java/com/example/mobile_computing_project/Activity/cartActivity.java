b0db8fbaf74b7ae7518e54ca4db105dcee485a97
preshiousshittu <54418932+preshiousshittu@users.noreply.github.com>
2021-11-20 00:18:39 -0600

    makes total visible

--- Mobile_Computing_project/app/src/main/java/com/example/mobile_computing_project/Activity/cartActivity.java
+++ Mobile_Computing_project/app/src/main/java/com/example/mobile_computing_project/Activity/cartActivity.java
@@ -77,7 +77,7 @@ public class cartActivity extends AppCompatActivity {
         });
 
         recyclerViewList.setAdapter(adapter);
-        if (managementCart.getListCard().isEmpty()) {
+        if (!managementCart.getListCard().isEmpty()) {
             emptyTxt.setVisibility(View.VISIBLE);
             //scrollView.setVisibility(View.GONE);
         } else {
@@ -100,6 +100,7 @@ public class cartActivity extends AppCompatActivity {
         taxTV.setText("$" + tax);
         deliveryTV.setText("$" + delivery);
         emptyTxt.setText("$" + total);
+        Log.d("total", " " + total);
     }
 
     private void initView() {
