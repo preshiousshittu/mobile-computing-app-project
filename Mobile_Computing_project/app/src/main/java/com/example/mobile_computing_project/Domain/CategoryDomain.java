19902fd30e6657d480138e85608c7a379c677661
ohilonge <54418880+ohilonge23@users.noreply.github.com>
2021-11-19 18:34:15 -0600

    Party Items and their descriptions included

--- Mobile_Computing_project/app/src/main/java/com/example/mobile_computing_project/Domain/CategoryDomain.java
+++ Mobile_Computing_project/app/src/main/java/com/example/mobile_computing_project/Domain/CategoryDomain.java
@@ -3,9 +3,14 @@ package com.example.mobile_computing_project.Domain;
 public class CategoryDomain {
     private String title;
     private String pic;
+    private String Description;
+    private double fee;
+    private int numberInCard;
 
-    public CategoryDomain(String title){
+
+    public CategoryDomain(String title, String description, double fee) {
         this.title = title;
+        this.Description = description;
         this.pic = pic;
     }
 
@@ -23,5 +28,29 @@ public class CategoryDomain {
 
     public void setPic(String pic) {
         this.pic = pic;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    public int getNumberInCart() {
+        return numberInCard;
+    }
+
+    public void setNumberInCart(int numberInCard) {
+        this.numberInCard = numberInCard;
     }
 }
